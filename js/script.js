bfd8d0d7528c717eb429d69dc1db3f58954883f3
Trotteriniku <stefanozidda3@gmail.com>
2023-10-12 10:00:01 +0200

    removed useless appends in js

--- js/script.js
+++ js/script.js
@@ -2,24 +2,20 @@ const wrapper =document.getElementById('boxwrapper')
 const max = 7
 for(let max = 1; max <= 100;  max++){
    
-   const boxEL =document.createElement('div')  
+   const boxEL =document.createElement('div') 
+    
     if(max % 3 ===0 && max % 5 === 0){
         boxEL.className='fizzbuzz'
-        wrapper.append(boxEL)
-        boxEL.innerHTML='fizzbuzz'
+        boxEL.innerHTML='fizzbuzz' 
     } else if(max % 3 === 0){
          boxEL.className='fizz'
-         wrapper.append(boxEL)
          boxEL.innerHTML='fizz'
     } else if(max % 5 === 0){
         boxEL.className='buzz'
-        wrapper.append(boxEL)
         boxEL.innerHTML='buzz'
     } else{
         boxEL.className='box'
+        boxEL.innerHTML=max  
+    }      
         wrapper.append(boxEL)
-        boxEL.innerHTML=max
-    }
 }
-
-
